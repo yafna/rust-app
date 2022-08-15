32ac8c7ee10584499fd6ad12e2f32e1720dada47
straw <straw@polvo.straw>
2022-08-15 12:05:59 -0400

    parsed defined square of pixels

--- src/nimage.rs
+++ src/nimage.rs
@@ -1,15 +1,42 @@
-use image::GenericImageView;
+use std::borrow::Borrow;
+use std::ops::BitXor;
 
-fn open_image() {
-    let img = image::open("files/part1.png").unwrap();
-// The dimensions method returns the images width and height.
-    println!("dimensions {:?}", img.dimensions());
-// The color method returns the image's `ColorType`.
-    println!("{:?}", img.color());
+use image::{DynamicImage, GenericImageView, Pixel, Rgba};
+
+struct Square {
+    nhash: i32,
+    count: i32,
+}
+
+fn parse_square(img: DynamicImage, i: u32, j: u32) -> i64 {
+    let mut gg = 0;
+    for xx in 0..30 {
+        for yy in 0..30 {
+            let mut line: String = "".to_owned();
+            for item in img.get_pixel(44 + i * 33 + xx, 44 + j * 33 + yy).to_rgb().channels().iter() {
+                let line2 = item.to_string().to_owned();
+                line.push_str(&line2);
+            }
+            let dd = line.parse::<i64>().unwrap();
+            gg = gg ^ dd;
+        }
+    }
+    gg
+}
+
+fn open_image() -> DynamicImage {
+    image::open("files/part1.png").unwrap()
 }
 
 #[cfg(test)]
 #[test]
 fn test_opening() {
-    open_image();
+    open_image(); // not panicking - good
+}
+
+#[test]
+fn test_parse_image() {
+    // let ir = (img.dimensions().0 - 44) / 33;
+    // let jr = (img.dimensions().1 - 44) / 33;
+    assert_eq!(254555242, parse_square(open_image(), 1, 1));
 }
